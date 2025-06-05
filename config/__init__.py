c922f955ed441374c596aef36aa6e34daa6bcf6e
Dishana <128571089+dishana11@users.noreply.github.com>
2025-06-05 15:21:14 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,9 @@
-
 """
 Configuration Package
 Handles all configuration settings for the Twitter bot.
 """
 
 from .settings import get_api_credentials, get_bot_config
-from .github_settings import get_github_config, load_config
+from .github_settings import get_github_config
 
-__all__ = ['get_api_credentials', 'get_bot_config', 'get_github_config', 'load_config']
+__all__ = ['get_api_credentials', 'get_bot_config', 'get_github_config']
